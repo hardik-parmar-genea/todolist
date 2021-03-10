5affac112ff50917d5a6855bae96639139277c58
hdkp98 <parmarhardik633@gmail.com>
2021-03-10 18:49:28 +0530

    first commit

---
+++ src/components/Todolist.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Todolist() {
+    
+    return (
+        <div className= 'todo-item'>
+            <input type="checkbox" />
+            <p> Placeholder text here </p>
+        </div>
+    )
+}
+
+export default Todolist;
